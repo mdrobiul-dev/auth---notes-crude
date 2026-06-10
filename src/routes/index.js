c81970f4a93232211b3a6a1fecd046bf9569461e
its-robii <robiulhassanrobi33@gmail.com>
2026-06-10 20:52:15 +0600

    maintainence

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
-import express from "express"
-import authRoutes from "./auth.route.js"
-import userRoutes from "./user.routes.js"
-import adminRoutes from "./admin.routes.js"
-import noteRoutes from "./note.route.js"
+import express from "express";
+import authRoutes from "./auth.route.js";
+import userRoutes from "./user.routes.js";
+import adminRoutes from "./admin.routes.js";
+import noteRoutes from "./note.route.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use("/auth", authRoutes)
-router.use("/user", userRoutes)
-router.use("/admin", adminRoutes)
-router.use("/notes", noteRoutes)
-   
-export default router                      
+router.use("/auth", authRoutes);
+router.use("/user", userRoutes);
+router.use("/admin", adminRoutes);
+router.use("/notes", noteRoutes);
+
+export default router;
