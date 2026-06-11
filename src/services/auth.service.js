a9c5b45a3c5b6280c5f374ad7ea12996ebe128c1
its-robii <robiulhassanrobi33@gmail.com>
2026-06-11 11:50:14 +0600

    generateToken typo fixed

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -61,7 +61,7 @@ export const refreshAccessToken = async (refreshToken) => {
 
   if (!user) throw new AppError('Invalid or expired refresh token', 401);
 
-  const newAccessToken = user.generateAuthToken();
+  const newAccessToken = user.generateToken();
   return { accessToken: newAccessToken, user };
 };
 
